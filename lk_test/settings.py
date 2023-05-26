da2f80ea84dde96666178596f2c714b8d84e8e04
er2fwqr <1356966372@qq.com>
2023-05-26 10:45:23 +0800

    delete

--- lk_test/settings.py
+++ lk_test/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+import os,sys
 from pathlib import Path
-
+sys.dont_write_bytecode=True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
